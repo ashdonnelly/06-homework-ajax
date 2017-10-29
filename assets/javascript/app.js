c4c0d11c4e42ae793d19cff81f89b697609782b3
ashdonnelly <ashdonnelly>
2017-10-28 20:59:58 -0600

    oh shi look who figured out how to replace spaces with +

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -55,7 +55,7 @@ function renderButtons() {
 		var a = $("<button>");
 
 		//add class
-		a.addClass("gif-button-new btn-sm btn-info");
+		a.addClass("gif-button-new btn btn-sm btn-info");
 
 		//add data-attribute with a value of gif at index i
 		a.attr("data-name"), gifsArray[i];
@@ -94,8 +94,11 @@ $("#submit-button").on("click", function() {
 	queryTerm = $("#search").val().trim();
 	console.log(queryTerm);
 
+	//replace spaces with +
+	// var replaced = queryTerm.replace(/ /g, "+");
+
 	// URL base + search term + auth key + limit results to 10 gifs + language english + size 200
-	var newURL = queryURLBase + "+" + queryTerm + authKey + "&limit=10&lang=en&fixed_height=200";
+	var newURL = queryURLBase + "+" + queryTerm.replace(/ /g, "+") + authKey + "&limit=10&lang=en&fixed_height=200";
 	console.log(newURL);
 	
 	return false;
